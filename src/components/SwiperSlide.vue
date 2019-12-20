6d13fef414daa091b1ac416f420ff03c7debf54e
lil-armstrong <oebong1@gmail.com>
2019-12-20 21:27:35 +0100

    v0.1.1

---
+++ src/components/SwiperSlide.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="swiper-slide">
+    <slot>
+      Slide content
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Swiper-slide'
+  }
+</script>
+
+<style scoped lang="scss">
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 10rem;
+    z-index: auto;
+  }
+</style>
